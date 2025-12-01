8a2cfa58b8b8ffadcc6eec366f0c9a4d83310a4d
Jordan Alexander Suwito <jordansuwito@gmail.com>
2025-12-01 13:01:46 +0700

    fix

--- app/src/main/java/uas/controllers/DeviceControllers.java
+++ app/src/main/java/uas/controllers/DeviceControllers.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import uas.models.Device;
-import uas.models.Fedora;
 import uas.models.IDeviceController;
 import uas.models.OperatingSystem;
-import uas.models.Ubuntu;
-import uas.models.Windows;
 
 public class DeviceControllers implements IDeviceController {
     private List<Device> listDevice;
@@ -17,30 +14,9 @@ public class DeviceControllers implements IDeviceController {
         this.listDevice = new ArrayList<>();
     }
 
-    // public void addDevice(String ownerName, String version, String os){
-    //     if (os.equalsIgnoreCase("Windows")) {
-    //         OperatingSystem operatingSystem = new Windows(version);
-    //         Device device = new Device(ownerName, operatingSystem);
-    //         this.listDevice.add(device);
-    //     }else if(os.equalsIgnoreCase("Ubuntu")){
-    //         OperatingSystem operatingSystem = new Ubuntu(version);
-    //         Device device = new Device(ownerName, operatingSystem);
-    //         this.listDevice.add(device);
-    //     }else{
-    //         OperatingSystem operatingSystem = new Fedora(version);
-    //         Device device = new Device(ownerName, operatingSystem);
-    //         this.listDevice.add(device);
-    //     }
-    // }
-
     public void addDevice(String ownerName, OperatingSystem os){
         this.listDevice.add(new Device(ownerName, os));
     }
-
-    // public void addDevice(String ownerName, String version, String desktopEnvironmentName){
-    //     // OperatingSystem operatingSystem = new Ubuntu(version,desktopEnvironmentName);
-    //     // OperatingSystem operatingSystem = new Fedora(version,desktopEnvironmentName);
-    // }
 
     public List<Device> getDeviceInfo(){
         return this.listDevice;
